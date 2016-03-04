33920801db087991e1f5ced95e47a2f423f55823
sikB <sikhovski@icloud.com>
2016-03-04 10:42:39 -0500

    updated
    dragon; still needs modifying

--- 3.3.16/dice.roller/Dragon.Game.js
+++ 3.3.16/dice.roller/Dragon.Game.js
@@ -8,6 +8,22 @@ function throwDie(){
 	var randomDie2 = Math.round(Math.random()*5)+1;
 	var imageName2 = "dice-img/d" + randomDie2 + ".gif";
 	document.images['myDie2'].src = imageName2;
+
+	var health = document.getElementById("yourhealth");
+	if((randomDie === 2) && (randomDie2 === 2)){
+	yourhealth.value= health.value - 30;}
+
+	if(health.value <= 0){
+		alert("You got burned to a crisp!!")}
+
+	var healthDragon = document.getElementById("dragonhealth");
+	if(randomDie === 2) {
+	dragonhealth.value = dragonhealth.value - 60;
+	if(dragonhealth.value <= 0){
+		alert("You've defeated the mighty Dragon!! Your life is spared")
+	}
+	
+}
 }
 
 
@@ -20,14 +36,13 @@ function runForYourLife(){
 	document.getElementById('dice').style.visibility = "hidden";
 	document.getElementById('dragon').style.visibility = "hidden";
 	document.getElementById('wild-dragon').style.visibility = "hidden";
-	document.getElementById('health').style.visibility = "hidden";
-	document.getElementsByClass('health-bar').style.visibility = "hidden";}
+	document.getElementById('your-health').style.visibility = "hidden";
+	document.getElementById('health-bar').style.visibility = "hidden";}
 
 
 
 
-// var health = document.getElementById("health");
-// health.value = health.value(- 20%);
+
 
 // if((['myDie1'] === "dice-img/d2.gif") && (['myDie2'] === "dice-img/d2.gif")){
 // 	health.value = "70"}
@@ -35,8 +50,6 @@ function runForYourLife(){
 
 //Whenever you lose health
 
-if((randomDie === 2) && (randomDie2 === 2)){
-	health-=30;}
-
+// 
 	
 
